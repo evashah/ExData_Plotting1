ef198543201b3aaa7901ac5f0a3ea8032558b010
Eva Shah <evashah@yahoo.com>
2016-03-21 01:22:13 -0400

    Added files via upload

---
+++ plot4.R
@@ -0,0 +1,46 @@
+# Exploratory Data Analysis
+# Coursera
+# John Hopkins University
+
+# Week 1 - Assignment for Peer Review - Plot 4
+
+# check / set working directory
+rm(list=ls())
+getwd()
+setwd("C:/Users/SV Tech Holdings/Documents/GitHub/ExData_Plotting1")
+
+#read data from text file
+url <- "household_power_consumption.txt"
+Data <- read.table(url, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+# subset data based on date
+subsetData <- Data[Data$Date %in% c("1/2/2007","2/2/2007") ,]
+rm(Data)
+summary(subsetData) # check the class for each column
+DateandTime <- strptime(paste(subsetData$Date, subsetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+GlobalActivePower <- as.numeric(subsetData$Global_active_power) # convert Global Active Power data to numeric
+GlobalReactivePower <- as.numeric(subsetData$Global_reactive_power) # convert Global Active Power data to numeric
+Voltage <- as.numeric(subsetData$Voltage) # convert Voltage data to numeric
+SubMetering1 <- as.numeric(subsetData$Sub_metering_1)
+SubMetering2 <- as.numeric(subsetData$Sub_metering_2)
+
+# plot all 4 graphs
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+# graph 1
+plot(DateandTime, GlobalActivePower, type="l", xlab="", ylab="Global Active Power")
+
+# graph 2
+plot(DateandTime, Voltage, type="l", xlab="datetime", ylab="Voltage")
+
+# graph 3
+plot(DateandTime, SubMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(DateandTime, SubMetering2, type="l", col="red")
+lines(DateandTime, subsetData$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"),bty="n")
+
+# graph 4
+plot(DateandTime, GlobalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power") # graph 4
+
+dev.off()
